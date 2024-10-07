54456f7d19963184a4c751531fcd4a8cf7195e87
JustSergioPB <justsergioit.com>
2024-10-07 23:06:58 +0200

    main: Patched migration

--- priv/repo/migrations/20241003163940_active_users_orgs.exs
+++ priv/repo/migrations/20241003163940_active_users_orgs.exs
@@ -2,16 +2,14 @@ defmodule NotebookServer.Repo.Migrations.ActiveUsersOrgs do
   use Ecto.Migration
 
   def change do
-    execute "DROP TYPE user_status CASCADE"
-    execute "CREATE TYPE user_status AS ENUM ('active', 'inactive')"
+    execute "CREATE TYPE user_status AS ENUM ('active', 'inactive')", "DROP TYPE user_status"
 
     alter table(:users) do
       add :status, :user_status, default: "active"
       add :confirmed_at, :utc_datetime
     end
 
-    execute "DROP TYPE org_status CASCADE"
-    execute "CREATE TYPE org_status AS ENUM ('active', 'inactive')"
+    execute "CREATE TYPE org_status AS ENUM ('active', 'inactive')", "DROP TYPE org_status"
 
     alter table(:orgs) do
       add :status, :org_status, default: "active"
