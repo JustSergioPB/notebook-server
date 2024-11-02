1a5b2996232c7e2135683625444cc6ea1e33b2ff
Sergio Peña <justsergioit@gmail.com>
2024-11-02 22:33:21 +0100

    main: Mail logs

--- lib/notebook_server/accounts/user_notifier.ex
+++ lib/notebook_server/accounts/user_notifier.ex
@@ -5,6 +5,13 @@ defmodule NotebookServer.Accounts.UserNotifier do
 
   # Delivers the email using the application mailer.
   defp deliver(recipient, subject, body) do
+    IO.inspect("================================")
+    IO.inspect(Application.get_env(:notebook_server, NotebookServer.Mailer)[:api_key])
+    IO.inspect(Application.get_env(:notebook_server, NotebookServer.Mailer)[:domain])
+    IO.inspect(Application.get_env(:notebook_server, NotebookServer.Mailer)[:from])
+    IO.inspect(Application.get_env(:notebook_server, NotebookServer.Mailer)[:email])
+    IO.inspect("================================")
+
     email =
       new()
       |> to(recipient)
