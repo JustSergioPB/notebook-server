ef22253a90491eee80a5eda96d04bfe4f2af52c9
Sergio Peña <justsergioit@gmail.com>
2024-11-02 22:13:06 +0100

    main: Removed migrations

--- priv/repo/migrations/20241101125646_schema_version_content_2.exs
+++
@@ -1,7 +0,0 @@
-defmodule NotebookServer.Repo.Migrations.SchemaVersionContent2 do
-  use Ecto.Migration
-
-  def change do
-    rename table(:schema_versions), :credential_subject, to: :content
-  end
-end
